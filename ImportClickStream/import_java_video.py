9b9ac851812bf5f58ab9f1d6dc831423039c7d10
mmylei <mmy_lei@hotmail.com>
2017-03-03 15:32:50 +0800

    new vision

--- ImportClickStream/import_java_video.py
+++ ImportClickStream/import_java_video.py
@@ -1,7 +1,10 @@
 import read_file
 import json_wrapper
+import re
 import MySQLdb
 import sys
+
+reg = re.compile(r'[0-9a-z]{32}')
 
 
 def create_table(conn, table):
@@ -12,7 +15,7 @@ def create_table(conn, table):
               "(user_name varchar(64), user_id int, event_source varchar(32), event_type varchar(256),"
               "event_time datetime, course_id varchar(128), `session` varchar(64), `current_time` decimal(10, 5),"
               "slide_type varchar(16), old_time decimal(10, 5), new_time decimal(10, 5), old_speed decimal(4, 2),"
-              "new_speed decimal(4, 2));")
+              "new_speed decimal(4, 2), saved_video_position int, video_id varchar(32));")
     conn.commit()
 
 
@@ -25,7 +28,19 @@ def insert_table(conn, fields, p, table):
 def truncate(date_time):
     return date_time[:date_time.find('.')]
 
+
+def extract_video_id(event_type):
+    return reg.search(event_type).group()
+
+
+def video_position_to_decimal(position):
+    parts = position.split(':')
+    return int(parts[0]) * 3600 + int(parts[1]) * 60 + int(parts[2])
+
 if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        print(extract_video_id('/courses/course-v1:HKUSTx+COMP102.1x+3T2016/xblock/block-v1:HKUSTx+COMP102.1x+3T2016+type@video+block@dfe488d381a542dfa8557bd069421e4a/handler/xmodule_handler/save_user_state'))
+        exit()
     dir = sys.argv[1]
     if not dir.endswith('/'):
         dir += '/'
@@ -48,14 +63,20 @@ if __name__ == '__main__':
         for row in read_file.read(file_name):
             row['time'] = truncate(row['time'])
             if row['event_type'] == 'load_video':
+                event = row['event']
+                if isinstance(event, str):
+                    event = json_wrapper.loads(event)
+                if 'currentTime' not in event:
+                    event['currentTime'] = None
                 try:
                     insert_table(conn,
                                  ['user_name', 'user_id', 'event_source', 'event_type',
-                                  'event_time', 'course_id', '`session`'],
+                                  'event_time', 'course_id', '`session`', 'video_id'],
                                  [row['username'], row['context']['user_id'], row['event_source'], row['event_type'],
-                                  row['time'], row['context']['course_id'], row['session']],
+                                  row['time'], row['context']['course_id'], row['session'], event['id']],
                                  table_name)
                 except Exception:
+                    print("exception found in term " + term)
                     print(row['time'])
                     raise
             elif row['event_type'] in ['play_video', 'pause_video', 'stop_video']:
@@ -67,11 +88,12 @@ if __name__ == '__main__':
                 try:
                     insert_table(conn,
                                  ['user_name', 'user_id', 'event_source', 'event_type',
-                                  'event_time', 'course_id', '`session`', '`current_time`'],
+                                  'event_time', 'course_id', '`session`', 'video_id', '`current_time`'],
                                  [row['username'], row['context']['user_id'], row['event_source'], row['event_type'],
-                                  row['time'], row['context']['course_id'], row['session'], event['currentTime']],
+                                  row['time'], row['context']['course_id'], row['session'], event['id'], event['currentTime']],
                                  table_name)
                 except Exception:
+                    print("exception found in term " + term)
                     print(row['time'])
                     raise
             elif row['event_type'] == 'seek_video':
@@ -83,11 +105,12 @@ if __name__ == '__main__':
                         event['old_time'] = None
                     insert_table(conn,
                                  ['user_name', 'user_id', 'event_source', 'event_type',
-                                  'event_time', 'course_id', '`session`', 'slide_type', 'old_time', 'new_time'],
+                                  'event_time', 'course_id', '`session`', 'video_id', 'slide_type', 'old_time', 'new_time'],
                                  [row['username'], row['context']['user_id'], row['event_source'], row['event_type'],
-                                  row['time'], row['context']['course_id'], row['session'], event['type'], event['old_time'], event['new_time']],
+                                  row['time'], row['context']['course_id'], row['session'], event['id'], event['type'], event['old_time'], event['new_time']],
                                  table_name)
                 except Exception:
+                    print("exception found in term " + term)
                     print(row['time'])
                     raise
             elif row['event_type'] == 'speed_change_video':
@@ -97,11 +120,28 @@ if __name__ == '__main__':
                 try:
                     insert_table(conn,
                                  ['user_name', 'user_id', 'event_source', 'event_type',
-                                  'event_time', 'course_id', '`session`', 'old_speed', 'new_speed'],
+                                  'event_time', 'course_id', '`session`', 'video_id', 'old_speed', 'new_speed'],
                                  [row['username'], row['context']['user_id'], row['event_source'], row['event_type'],
-                                  row['time'], row['context']['course_id'], row['session'], event['old_speed'],
+                                  row['time'], row['context']['course_id'], row['session'], event['id'], event['old_speed'],
                                   event['new_speed']],
                                  table_name)
                 except Exception:
+                    print("exception found in term " + term)
+                    print(row['time'])
+                    raise
+            elif row['event_type'].endswith("save_user_state"):
+                event = row['event']
+                if isinstance(event, str):
+                    event = json_wrapper.loads(event)
+                try:
+                    insert_table(conn,
+                                 ['user_name', 'user_id', 'event_source', 'event_type',
+                                  'event_time', 'course_id', 'video_id', 'saved_video_position'],
+                                 [row['username'], row['context']['user_id'], row['event_source'], 'save_user_state',
+                                  row['time'], row['context']['course_id'], extract_video_id(row['event_type']),
+                                  video_position_to_decimal(event['POST']['saved_video_position'][0]), event['new_speed']],
+                                 table_name)
+                except Exception:
+                    print("exception found in term " + term)
                     print(row['time'])
                     raise
