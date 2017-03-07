34a9d2ff859ec4b7ddc05ea5edcfb682f6c15496
mmylei <mmy_lei@hotmail.com>
2017-03-07 20:05:25 +0800

    bug fixed

--- xml2mysql/problem.py
+++ xml2mysql/problem.py
@@ -10,7 +10,7 @@ def create_problem_table(conn, table):
     conn.commit()
     c.execute("CREATE TABLE " + table + " "
               "(`course_id` varchar(20), term_id varchar(20), xml_id varchar(50), "
-                "display_name varchar(100), problem_type varchar(50);")
+                "display_name varchar(100), problem_type varchar(50));")
     conn.commit()
 
 
