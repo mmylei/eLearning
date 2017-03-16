ae72c44ba809903859f571c22e71c05b6c05e1e0
mmylei <mmy_lei@hotmail.com>
2017-03-16 14:32:25 +0800

    debug

--- ImportClickStream/create_video_play_piece.py
+++ ImportClickStream/create_video_play_piece.py
@@ -9,7 +9,7 @@ def create_table(conn, table):
     c.execute("CREATE TABLE " + table + " "
               "(`session` varchar(64), user_id int, video_id varchar(64), "
               "video_time_start decimal(10, 5) not null, video_time_end decimal(10, 5) not null, "
-              "real_time_start datetime, real_time_end datetime);")
+              "real_time_start datetime, real_time_end datetime, finished int not null default 0);")
     conn.commit()
 
 
@@ -27,27 +27,27 @@ if __name__ == '__main__':
 
     terms = [
         # java
-        'COMP102.1x-2T2015',
-        'COMP102.1x-2T2016',
-        'COMP102.1x-3T2016',
+        # 'COMP102.1x-2T2015',
+        # 'COMP102.1x-2T2016',
+        # 'COMP102.1x-3T2016',
         'COMP102.1x-4T2015',
-        'COMP102.2x-1T2016',
-        'COMP102.2x-2T2016',
-        'COMP102.2x-3T2016',
-        'COMP102.2x-4T2015',
-        'COMP102x-2T2014',
+        # 'COMP102.2x-1T2016',
+        # 'COMP102.2x-2T2016',
+        # 'COMP102.2x-3T2016',
+        # 'COMP102.2x-4T2015',
+        # 'COMP102x-2T2014',
         # android
-        'COMP107x-3T2016',
-        'COMP107x-2016_T1',
-        'COMP107x-1T2016',
+        # 'COMP107x-3T2016',
+        # 'COMP107x-2016_T1',
+        # 'COMP107x-1T2016',
         # speaking
-        'EBA101x-3T2016',
-        'EBA101x-3T2014',
-        'EBA101x-1T2016',
+        # 'EBA101x-3T2016',
+        # 'EBA101x-3T2014',
+        # 'EBA101x-1T2016',
         # writing
-        'EBA102x-4Q2015',
-        'EBA102x-3T2016',
-        'EBA102x-1T2016'
+        # 'EBA102x-4Q2015',
+        # 'EBA102x-3T2016',
+        # 'EBA102x-1T2016'
     ]
     conn = MySQLdb.connect(host="localhost", user="eLearning", passwd="Mdb4Learn", db="clickstream")
     for term in terms:
@@ -85,7 +85,7 @@ if __name__ == '__main__':
                 if session is not None and session != current_session:
                     # make up plays without `stop` or `pause` event
                     for video_id in temporary:
-                        cursor.execute('INSERT INTO ' + table_name2 + ' VALUES(%s, %s, %s, %s, %s, %s, %s);',
+                        cursor.execute('INSERT INTO ' + table_name2 + ' VALUES(%s, %s, %s, %s, %s, %s, %s, 0);',
                                        [current_session, user_id, video_id, playing[video_id], temporary[video_id],
                                         event_start[video_id],
                                         temporary_time[video_id]])
@@ -108,8 +108,9 @@ if __name__ == '__main__':
                         temporary_time[video_id] = event_time
                 elif event_type in ['stop_video', 'pause_video'] and current_time is not None:
                     if video_id in playing:
-                        cursor.execute('INSERT INTO ' + table_name2 + ' VALUES(%s, %s, %s, %s, %s, %s, %s);',
-                                       [session, user_id, video_id, playing[video_id], current_time, event_start[video_id], event_time])
+                        finished = 1 if event_type == 'stop_video' else 0
+                        cursor.execute('INSERT INTO ' + table_name2 + ' VALUES(%s, %s, %s, %s, %s, %s, %s, %s);',
+                                       [session, user_id, video_id, playing[video_id], current_time, event_start[video_id], event_time, finished])
                         del playing[video_id]
                         del event_start[video_id]
                         if video_id in temporary:
@@ -117,13 +118,13 @@ if __name__ == '__main__':
                 elif event_type == 'seek_video':
                     if video_id in playing and row_user[3] is not None:
                         old_time = float(row_user[3])
-                        cursor.execute('INSERT INTO ' + table_name2 + ' VALUES(%s, %s, %s, %s, %s, %s, %s);',
+                        cursor.execute('INSERT INTO ' + table_name2 + ' VALUES(%s, %s, %s, %s, %s, %s, %s, 0);',
                                        [session, user_id, video_id, playing[video_id], old_time, event_start[video_id], event_time])
                         if video_id in temporary:
                             del temporary[video_id]
                             del playing[video_id]
                     elif video_id in playing and video_id in temporary:
-                        cursor.execute('INSERT INTO ' + table_name2 + ' VALUES(%s, %s, %s, %s, %s, %s, %s);',
+                        cursor.execute('INSERT INTO ' + table_name2 + ' VALUES(%s, %s, %s, %s, %s, %s, %s, 0);',
                                        [current_session, user_id, video_id, playing[video_id], temporary[video_id], event_start[video_id],
                                         temporary_time[video_id]])
                         del temporary[video_id]
@@ -135,17 +136,17 @@ if __name__ == '__main__':
                         new_time = float(row_user[4])
                         playing[video_id] = new_time
                         event_start[video_id] = event_time
-                elif event_type == 'save_user_state' and row_user[5] is not None:
-                    if video_id not in playing:
-                        playing[video_id] = float(row_user[5])
-                        event_start[video_id] = event_time
-                    elif playing[video_id] != row_user[5]:
-                        temporary[video_id] = float(row_user[5])
-                        temporary_time[video_id] = event_time
+                # elif event_type == 'save_user_state' and row_user[5] is not None:
+                #     if video_id not in playing:
+                #         playing[video_id] = float(row_user[5])
+                #         event_start[video_id] = event_time
+                #     elif playing[video_id] != row_user[5]:
+                #         temporary[video_id] = float(row_user[5])
+                #         temporary_time[video_id] = event_time
 
             # make up plays without `stop` or `pause` event
             for video_id in temporary:
-                cursor.execute('INSERT INTO ' + table_name2 + ' VALUES(%s, %s, %s, %s, %s, %s, %s);',
+                cursor.execute('INSERT INTO ' + table_name2 + ' VALUES(%s, %s, %s, %s, %s, %s, %s, 0);',
                                [current_session, user_id, video_id, playing[video_id], temporary[video_id], event_start[video_id],
                                 temporary_time[video_id]])
 
