682f5d363213169ba939638db25e3e70fbad3f95
mmylei <mmy_lei@hotmail.com>
2017-04-03 16:21:22 +0800

    adjust picture

--- ImportClickStream/draw_video_watch.py
+++ ImportClickStream/draw_video_watch.py
@@ -8,7 +8,7 @@ import numpy as np
 import os
 
 width = 1024
-height = 512
+height = 256
 my_dpi = 256
 # max_watching = 26665.0
 dir = "./"
@@ -39,7 +39,7 @@ def draw(video_id, duration, watchs):
             data[0][i] = 0
 
     plt.figure(figsize=(width / my_dpi, height / my_dpi), dpi=my_dpi)
-    fig = plt.imshow(data, extent=(0, width, 0, height), cmap=cm.plasma)
+    fig = plt.imshow(data, extent=(0, seconds, 0, height), cmap=cm.plasma)
     # plt.axis('off')
     # fig.axes.get_xaxis().set_visible(False)
     fig.axes.get_yaxis().set_visible(False)
