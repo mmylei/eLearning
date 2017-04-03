efe4d36d24a85345c0d3b1de3c491742c602fdc6
mmylei <mmy_lei@hotmail.com>
2017-04-03 16:03:40 +0800

    add x axis

--- ImportClickStream/draw_video_watch.py
+++ ImportClickStream/draw_video_watch.py
@@ -40,9 +40,10 @@ def draw(video_id, duration, watchs):
 
     plt.figure(figsize=(width / my_dpi, height / my_dpi), dpi=my_dpi)
     fig = plt.imshow(data, extent=(0, width / my_dpi, 0, height / my_dpi), cmap=cm.plasma)
-    plt.axis('off')
-    fig.axes.get_xaxis().set_visible(False)
+    # plt.axis('off')
+    # fig.axes.get_xaxis().set_visible(False)
     fig.axes.get_yaxis().set_visible(False)
+    plt.xlabel('video time')
     plt.savefig(dir + video_id + '.png', format='png', bbox_inches='tight', pad_inches=0)
 
 
