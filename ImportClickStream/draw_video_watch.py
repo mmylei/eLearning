8f987c8e8aa118e70d0a079f0a8e56ae000bf4b6
mmylei <mmy_lei@hotmail.com>
2017-04-03 16:23:41 +0800

    adjust picture

--- ImportClickStream/draw_video_watch.py
+++ ImportClickStream/draw_video_watch.py
@@ -39,7 +39,7 @@ def draw(video_id, duration, watchs):
             data[0][i] = 0
 
     plt.figure(figsize=(width / my_dpi, height / my_dpi), dpi=my_dpi)
-    fig = plt.imshow(data, extent=(0, seconds, 0, height), cmap=cm.plasma)
+    fig = plt.imshow(data, extent=(0, seconds, 0, seconds * height / width), cmap=cm.plasma)
     # plt.axis('off')
     # fig.axes.get_xaxis().set_visible(False)
     fig.axes.get_yaxis().set_visible(False)
