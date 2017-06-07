ddc5f42d4cf928d08bdd1ed1d1abfe008c5a9bb1
mmylei <mmy_lei@hotmail.com>
2017-06-07 12:42:18 +0800

    bug fix

--- eLearning_analysis/student_problem_grade.py
+++ eLearning_analysis/student_problem_grade.py
@@ -1,12 +1,12 @@
 import MySQLdb
 
 sql_template = 'create table $term$_student_grade as ' \
-      'select G.student_id as student_id, P.aggregated_category as category, sum(grade) as grade' \
+      'select G.student_id as student_id, aggregated_category, sum(grade) as grade' \
       ' from all_students_grades as G, $term$_problem_set as P' \
       ' where G.course_id = P.course_id and G.term_id = P.term_id and G.xml_id = P.xml_id' \
-      ' group by student_id, category;'
+      ' group by student_id, aggregated_category;'
 
-terms = ['HKUSTx_COMP102.1x-4T2015']
+terms = ['HKUSTx_COMP102_1x_4T2015']
 conn = MySQLdb.connect(host="localhost", user="eLearning", passwd="Mdb4Learn", db="eLearning")
 for term in terms:
     sql = sql_template.replace('$term$', term)
