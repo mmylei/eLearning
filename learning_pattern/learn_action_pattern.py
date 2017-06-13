2de3ccb5e3c225bc458ec0da27de39a345a6c394
mmylei <mmy_lei@hotmail.com>
2017-06-13 15:02:35 +0800

    cross validation

--- learning_pattern/learn_action_pattern.py
+++ learning_pattern/learn_action_pattern.py
@@ -3,22 +3,40 @@ from sklearn.model_selection import KFold
 from sklearn.linear_model import SGDClassifier
 from sklearn import tree
 from sklearn.metrics import mean_squared_error
+from sklearn.preprocessing import StandardScaler
+
 import json_wrapper
 
 f = open('data.json', 'r')
 data = json_wrapper.loads(f.read())
 f.close()
+test_error = 0
+train_error = 0
+X = np.array(data['features'], np.intp)
+Y = np.array(data['labels'], np.intp)
+for train_index, test_index in KFold(n_splits=5).split(X):
+    X_train, X_test = X[train_index], X[test_index]
+    Y_train, Y_test = Y[train_index], Y[test_index]
+    scaler = StandardScaler().fit(X_train)
+    X_train = scaler.transform(X_train)
+    X_test = scaler.transform(X_test)
+# train_x = features[data['train_index'], :]
+# train_y = labels[data['train_index']]
+# test_x = features[data['test_index'], :]
+# test_y = labels[data['test_index']]
 
-features = np.array(data['features'], np.intp)
-labels = np.array(data['labels'], np.intp)
-train_x = features[data['train_index'], :]
-train_y = labels[data['train_index']]
-test_x = features[data['test_index'], :]
-test_y = labels[data['test_index']]
+    model = SGDClassifier()
+    #model = tree.DecisionTreeClassifier()
+    model.fit(X_train, Y_train)
+    Y_predict = model.predict(X_test)
+    error = mean_squared_error(Y_test, Y_predict)
+    test_error += error
+    Y_predict = model.predict(X_train)
+    error = mean_squared_error(Y_train, Y_predict)
+    train_error += error
+real_test_error = test_error/5
+real_train_error = train_error/5
 
-model = SGDClassifier()
-#model = tree.DecisionTreeClassifier()
-model.fit(train_x, train_y)
-predict_y = model.predict(test_x)
-error = mean_squared_error(test_y, predict_y)
-print 'error=', error
+print 'train_error=', real_train_error
+print 'test_error=', real_test_error
+
