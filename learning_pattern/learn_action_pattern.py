dd7cbf39f2165882267f19311df3b70c2c8b1c93
mmylei <mmy_lei@hotmail.com>
2017-06-14 10:17:04 +0800

    add logger

--- learning_pattern/learn_action_pattern.py
+++ learning_pattern/learn_action_pattern.py
@@ -5,18 +5,24 @@ from sklearn.linear_model import SGDClassifier
 from sklearn import tree
 from sklearn.metrics import mean_squared_error
 from sklearn.preprocessing import StandardScaler
-
+import logging
 import json_wrapper
+
+FORMAT = '%(asctime)-15s [%(levelname)s] %(filename)s:%(lineno)d %(message)s'
+logging.basicConfig(format=FORMAT)
+logger = logging.getLogger('root')
 
 
 def feature_selection(X, Y):
     return SelectKBest(mutual_info_classif, 20).fit_transform(X, Y)
 
 
-def train(X, Y, model=SGDClassifier(penalty='l1', alpha=0.1)):
+def train(X, Y, model=SGDClassifier(penalty='l1', alpha=0.01)):
     test_error = 0
     train_error = 0
     for train_index, test_index in KFold(n_splits=5).split(X):
+        logger.info('train index: ' + str(train_index))
+        logger.info('test index: ' + str(test_index))
         X_train, X_test = X[train_index], X[test_index]
         Y_train, Y_test = Y[train_index], Y[test_index]
         # model = tree.DecisionTreeClassifier()
@@ -30,8 +36,8 @@ def train(X, Y, model=SGDClassifier(penalty='l1', alpha=0.1)):
     real_test_error = test_error / 5
     real_train_error = train_error / 5
 
-    print 'train_error:', real_train_error
-    print 'test_error:', real_test_error
+    logger.info('train_error: ' + str(real_train_error))
+    logger.info('test_error: ' + str(real_test_error))
     return model
 
 if __name__ == '__main__':
@@ -44,7 +50,9 @@ if __name__ == '__main__':
     Y = np.array(data['labels'], np.intp)
     models = []
     for label in [-1, 0, 1]:
-        print 'train for label', label
+        logger.info('train for label ' + str(label))
         Y_one = map(lambda x: 1 if x == label else 0, Y)
+        logger.info('feature selection')
         X_one = feature_selection(X, Y_one)
+        logger.info('training')
         models.append(train(X_one, Y_one))
