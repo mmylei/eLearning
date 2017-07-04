99d8d602e29c421acb479ef23e68b5b27598929f
mmylei <mmy_lei@hotmail.com>
2017-07-04 15:26:37 +0800

    lasso

--- learning_pattern/learn_action_pattern.py
+++ learning_pattern/learn_action_pattern.py
@@ -67,7 +67,7 @@ def feature_selection(X, Y):
     # return KernelPCA(n_components=50, kernel='rbf').fit_transform(X)
     scaler = StandardScaler().fit(X)
     X = scaler.transform(X)
-    return RFECV(svm.SVR(kernel="linear"), cv=5, step=0.05).fit_transform(X, Y)
+    return RFECV(Lasso(), cv=5, step=0.05).fit_transform(X, Y)
 
 
 def split_train_test(data):
