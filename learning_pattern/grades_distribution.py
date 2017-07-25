39ba5a3f5c4a1d163f984c91ad15d32e9698ef05
mmylei <mmy_lei@hotmail.com>
2017-07-25 15:26:54 +0800

    bug fix

--- learning_pattern/grades_distribution.py
+++ learning_pattern/grades_distribution.py
@@ -9,13 +9,7 @@ cursor = conn.cursor()
 cursor.execute(sql)
 data = cursor.fetchall()
 grade = map(lambda x: x[1], data)
-mean = 0
-sigma = 1
-x = grade
-fig, (ax0, ax1) = plt.subplots(nrows=2, figsize=(9, 6))
-ax0.hist(x, 40, normed=1, histtype='bar', facecolor='yellowgreen', alpha=0.75)
-ax0.set_title('pdf')
-ax1.hist(x, 20, normed=1, histtype='bar', facecolor='pink', alpha=0.75, cumulative=True, rwidth=0.8)
-ax1.set_title("cdf")
-fig.subplots_adjust(hspace=0.4)
+plt.hist(grade, bins=[0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0])
+plt.title("grades distribution")
+plt.show()
 plt.savefig('grades_distribution.png', dpi=120)
