3933071bcc2807d3e655051a29e2831032164892
mmylei <mmy_lei@hotmail.com>
2017-08-02 17:27:33 +0800

    week04 grade distribution

--- learning_pattern/grades_distribution.py
+++ learning_pattern/grades_distribution.py
@@ -7,7 +7,7 @@ from matplotlib import pyplot as plt
 
 conn = MySQLdb.connect(host="localhost", user="eLearning", passwd="Mdb4Learn", db="eLearning")
 # sql = 'select user_id, grade from 102_1x_4T2015_certificates_generatedcertificate;'
-sql = 'select student_id, sum(grade) from HKUSTx_COMP102_1x_4T2015_student_grade where aggregated_category like \'%03\' group by student_id;'
+sql = 'select student_id, sum(grade) from HKUSTx_COMP102_1x_4T2015_student_grade where aggregated_category like \'%04\' group by student_id;'
 cursor = conn.cursor()
 cursor.execute(sql)
 data = cursor.fetchall()
@@ -15,4 +15,4 @@ grade = map(lambda x: float(x[1]), data)
 plt.hist(grade, bins=range(0, 20))
 plt.title("grades distribution")
 # plt.show()
-plt.savefig('week03_grades_distribution.png', dpi=120)
+plt.savefig('week04_grades_distribution.png', dpi=120)
