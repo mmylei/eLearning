c6b3f6616b42923de559fb793ef1073005c25f85
mmylei <mmy_lei@hotmail.com>
2017-08-25 10:07:28 +0800

    bug fix

--- learning_pattern/draw_video_action_data.py
+++ learning_pattern/draw_video_action_data.py
@@ -92,6 +92,7 @@ for week_number in range(1, 6):
     Y = week_df['grade'].values
     Y = np.digitize(Y, bins[week_number-1])
     data_proj = TSNE(random_state=RS).fit_transform(X)
+    data_proj = np.clip(data_proj, 0, 1000)
     scatter(data_proj, Y)
     plt.savefig('tsne-generated_' + str(week_number) + '.png', dpi=120)
 
