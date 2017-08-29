a1c183df38914b801c411f027964cc0fe4af5127
mmylei <mmy_lei@hotmail.com>
2017-08-29 10:16:42 +0800

    bug fix

--- learning_pattern/outlier_detection.py
+++ learning_pattern/outlier_detection.py
@@ -28,20 +28,23 @@ for week_number in range(1, 6):
     # Y = features[indices, :][:, len(columns)-3]  # grade (last column)
     # X = features[indices, :][:, range(1, len(columns)-3)]  # except module_number (first column) and grade (last column)
     week_df = df[df.module_number == week_number]
+    week_df = drop_long_real_spent_row(week_df).reset_index()
+    idx = non_0_row_index(week_df[['real_spent', 'coverage', 'watched', 'pauses', 'pause_length', 'avg_speed', 'std_speed', 'seek_backward', 'seek_forward']])
+    week_df = week_df[idx].reset_index()
     week_df = week_df.groupby('uid') \
         .agg({'real_spent': 'mean', 'coverage': 'mean', 'watched': 'mean', 'pauses': 'mean',
               'pause_length': 'mean', 'avg_speed': 'mean', 'std_speed': 'mean',
               'seek_backward': 'mean', 'seek_forward': 'mean', 'attempts': 'max', 'grade': 'max'}) \
         .reset_index()
     # week_df = drop_all_0_row(week_df)
-    week_df = drop_long_real_spent_row(week_df).reset_index()
+    # week_df = drop_long_real_spent_row(week_df).reset_index()
     X = week_df[
         ['real_spent', 'coverage', 'watched', 'pauses', 'pause_length', 'avg_speed', 'std_speed', 'seek_backward',
-         'seek_forward']]
-    idx = non_0_row_index(X)
-    X = X[idx].values
+         'seek_forward']].values
+    # idx = non_0_row_index(X)
+    # X = X[idx].values
     X = scaler.fit_transform(X)
-    week_df = week_df[idx].reset_index()
+    # week_df = week_df[idx].reset_index()
     if week_number == 1:
         inlier_uid = [96243, 126901, 171082, 283682, 200160, 79486, 83536, 96243, 10364885, 126493, 5437, 9016245, 8959747, 7911021, 10320236, 10286875, 10019464, 9481638, 9016245, 9044224, 7911021, 7865357, 1764726, 6209505]
         inlier = week_df[week_df['uid'].isin(inlier_uid)][['real_spent', 'coverage', 'watched', 'pauses', 'pause_length', 'avg_speed', 'std_speed', 'seek_backward',
