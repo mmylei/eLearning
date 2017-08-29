2aa175a606200127f82e8e3cb37ad3bcbdbd41a3
mmylei <mmy_lei@hotmail.com>
2017-08-29 10:31:36 +0800

    avg_speed > 0.5

--- learning_pattern/outlier_detection.py
+++ learning_pattern/outlier_detection.py
@@ -55,7 +55,7 @@ for week_number in range(1, 6):
         Y = clf.predict(X)
         print 'outliers:'
         temp = week_df[Y == -1]
-        temp = temp[temp['avg_speed'] > 0.0001]
+        temp = temp[temp['avg_speed'] > 0.5]
         print temp
     # md = DBSCAN(eps=1.5, min_samples=10)
     # md.fit(X)
