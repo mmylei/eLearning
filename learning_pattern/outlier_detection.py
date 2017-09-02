addbae488241cccb9c373135e54921fb0af661f9
mmylei <mmy_lei@hotmail.com>
2017-09-02 19:01:55 +0800

    print cluster center

--- learning_pattern/outlier_detection.py
+++ learning_pattern/outlier_detection.py
@@ -70,13 +70,18 @@ for week_number in range(1, 6):
     # print week_df[md.labels_ == -1]
     X = scaler.fit_transform(X)
     X = X[Y == 1]
-    inertia = []
-    for n in range(1, 11):
-        kmeans = KMeans(n_clusters=n, random_state=0).fit(X)
-        # print kmeans.cluster_centers_
-        print kmeans.inertia_
-        inertia.append(kmeans.inertia_)
-    fig = plt.figure()
-    plt.plot(inertia)
-    savefig('./inertia_week_' + str(week_number) + '.eps', bbox_inches='tight')
-    plt.close()
+
+    # draw inertia plots for choosing cluster number
+    # inertia = []
+    # for n in range(1, 11):
+    #     kmeans = KMeans(n_clusters=n, random_state=0).fit(X)
+    #     # print kmeans.cluster_centers_
+    #     print kmeans.inertia_
+    #     inertia.append(kmeans.inertia_)
+    # fig = plt.figure()
+    # plt.plot(inertia)
+    # savefig('./inertia_week_' + str(week_number) + '.eps', bbox_inches='tight')
+    # plt.close()
+
+    kmeans = KMeans(n_clusters=4, random_state=0).fit(X)
+    print kmeans.cluster_centers_
