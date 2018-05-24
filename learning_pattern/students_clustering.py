5d9a75b72c17ccea5ba78a40134a52955e3e2e48
mmylei <mmy_lei@hotmail.com>
2018-05-24 21:11:50 +0800

    debug

--- learning_pattern/students_clustering.py
+++ learning_pattern/students_clustering.py
@@ -3,8 +3,23 @@ from sklearn.cluster import KMeans
 import MySQLdb
 import os
 import csv
+from sklearn.manifold import TSNE
+RS = 20150101
 from sklearn.feature_selection import SelectKBest
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
+import matplotlib
+matplotlib.use('agg')
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set_style('darkgrid')
+sns.set_palette('muted')
+sns.set_context("notebook", font_scale=1.5,
+                rc={"lines.linewidth": 2.5})
+import logging
+
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s [%(levelname)s] (%(name)s: %(lineno)d) %(message)s')
 
 conn = MySQLdb.connect(host="localhost", user="eLearning", passwd="Mdb4Learn", db="clickstream")
 
@@ -166,35 +181,52 @@ def get_replay():
 
 
 def prepare_features():
+    logger.info("prepare features")
     for term_key in terms:
         if os.path.exists(term_key + '_assignment_stats_features.csv'):
             continue
+        logger.info("term " + term_key)
         term = term_key.replace('.', '_').replace('-', '_')
+        logger.info("get replay data")
+        replay = get_replay()
+        logger.info("get initial data")
         cursor = conn.cursor()
         cursor.execute("select A.student_id, A.module_id, A.module_name from eLearning." + term +
                        "_assignment_stats as A, eLearning." + term +
                        "_django_comment_client_role_users as R, HKUSTx_" + terms[term_key]
                    + term_key.replace('.', '_').replace('-', '_') + "_video_play_piece as P where A.student_id = R.user_id and A.student_id = P.user_id and A.student_id <> 0 "
                        "and R.name = 'Student';")
+        initial_data = cursor.fetchall()
+        logger.info("get feature for each student")
         features = []
-        replay = get_replay()
-        for row in cursor.fetchall():
+        for row in initial_data:
             user_id = str(row[0])
             module_id = row[1]
             module_name = row[2]
             module_number = int(row[2].split('0')[-1]) if '0' in row[2] else -10
+            logger.info("get average watch time")
             avg_watch_time = get_avg_watch_time(cursor, user_id, module_number, term_key)
+            logger.info("get average solve time")
             avg_solve_time = get_avg_solve_time(cursor, user_id, module_id, term, module_name)
+            logger.info("get average watch num")
             avg_watch_num = get_avg_watch_num(cursor, user_id, module_number, term_key)
+            logger.info("get complete time")
             complete_time = get_complete_time(cursor, user_id, module_id, module_number, term_key)
+            logger.info("get average replay times")
             avg_replay_times = get_avg_replay_times(user_id, module_name, replay)
+            logger.info("get average submit times")
             avg_submit_times = get_avg_submit_times(cursor, user_id, module_id, term, module_name)
+            logger.info("get grades")
             grades = get_grades(cursor, user_id, term, module_name, term_key)
+            logger.info("get correct num")
             correct_num = get_correct_num(cursor, user_id, term, module_id, module_name, term_key)
+            logger.info("get forum activity")
             forum_activity = get_forum_activity(cursor, user_id, term, module_name)
+            logger.info("user done")
             one_feature = [user_id, module_id, module_name, avg_watch_time, avg_solve_time, avg_watch_num, complete_time,
                            avg_replay_times, avg_submit_times, grades, correct_num, forum_activity]
             features.append(one_feature)
+        logger.info("writing features")
         with open(term_key + '_assignment_stats_features.csv', 'wb') as f:
             writer = csv.writer(f)
             for feature in features:
@@ -241,8 +273,47 @@ def get_correlation():
         scores = correlation.scores_
         print scores
 
+
+def scatter(x, colors):
+    # We choose a color palette with seaborn.
+    palette = np.array(sns.color_palette("hls", 10))
+    # We create a scatter plot.
+    f = plt.figure(figsize=(8, 8))
+    ax = plt.subplot(aspect='equal')
+    sc = ax.scatter(x[:,0], x[:,1], lw=0, s=40,
+                    c=palette[colors.astype(np.int)])
+    plt.xlim(-25, 25)
+    plt.ylim(-25, 25)
+    ax.axis('off')
+    ax.axis('tight')
+
+
+def draw():
+    for term_key in terms:
+        print term_key
+        features = []
+        labels = []
+        scaler = MinMaxScaler()
+        with open(term_key + '_assignment_stats_features.csv', 'rb') as f:
+            reader = csv.reader(f)
+            for row in reader:
+                features.append(row[3:])
+        np_features = np.array(features, dtype=np.float32)
+        np_features = scaler.fit_transform(np_features)
+        with open(term_key + '_assignment_stats_KMeans.csv', 'rb') as f:
+            reader = csv.reader(f)
+            for row in reader:
+                labels.append(row[0])
+        np_labels = np.array(labels, dtype=np.float32)
+        data_proj = TSNE(random_state=RS).fit_transform(np_features)
+        data_proj = np.clip(data_proj, -1000, 1000)
+        scatter(data_proj, np_labels)
+        plt.savefig('tsne-generated_clusters_' + term_key + '.png', dpi=120)
+
+
 if __name__ == '__main__':
     prepare_features()
     conn.close()
     clustering()
     get_correlation()
+    draw()
